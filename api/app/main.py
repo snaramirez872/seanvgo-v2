15aba3668cb61ce591d1cafa0f01cf5e63f7a43e
snaramirez872 <sean.ramirez.dev@gmail.com>
2026-01-30 20:00:00 -0500

    implemeted basic back end

--- api/app/main.py
+++ api/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routes.health import router as health_router
+from app.routes.games import router as games_router
 
 app = FastAPI(title="SeanVGO v2 API")
 
@@ -9,7 +10,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",              # local dev frontend
-        "https://your-frontend.vercel.app"   # deployed frontend
+        "https://seanvgo-v2.vercel.app"   # deployed frontend
     ],
     allow_credentials=True,
     allow_methods=["*"],
@@ -25,3 +26,4 @@ def root():
 
 # Other Routes
 app.include_router(health_router, prefix="/api")
+app.include_router(games_router, prefix="/api")
---
+++ api/app/routes/games.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter, HTTPException
+from app.core.config import supabase
+
+router = APIRouter()
+
+@router.get("/games", tags=["games"])
+def get_games_list():
+    try:
+        resp = supabase.table("games_list").select("*").execute()
+
+        return {
+            "count": len(resp.data),
+            "data": resp.data
+        }
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail=str(e)
+        )
